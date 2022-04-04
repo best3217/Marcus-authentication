51a459931a0f7b9861f4ed074b600968a3d783ca
pdev217 <kjav.spt@gmail.com>
2022-04-04 16:18:18 +0300

    initial

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -7,7 +7,7 @@ const Welcome = () => {
         <>
             <h1 className="text-center">Hi Marcus</h1>
             <div>
-                <div className="text-center mb-4">You logged in with <strong>{user}</strong></div>
+                <div className="text-center mb-4">You were logged in with <strong>{user}</strong></div>
             </div>
             <div className="text-center">
                 <button onClick={logout} className="btn btn-danger">Sign Out</button>
